5b1d64f01bcfb4ada06a196ef8bad418b9449495
unknown <wooojin401@gmail.com>
2025-08-04 16:43:42 +0900

    asd

--- src/main/java/com/example/uscoding/PostController.java
+++ src/main/java/com/example/uscoding/PostController.java
@@ -9,7 +9,7 @@ public class PostController {
 
     @PostMapping
     public void receivePost(@RequestBody PostRequestDto     dto) {
-        System.out.println("주제: " + dto.getTitle());
+        System.out.println("주제2: " + dto.getTitle());
         System.out.println("코드: " + dto.getCode());
         System.out.println("설명: " + dto.getContent());
         System.out.println("카테고리: " + dto.getCategory());
