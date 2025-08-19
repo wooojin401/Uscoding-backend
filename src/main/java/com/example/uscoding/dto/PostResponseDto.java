1c722f2993c6a5117a19fa07f6487337c298b0e7
poroli0529 <kafaprl@gmail.com>
2025-08-19 17:08:44 +0900

    commit0819

--- src/main/java/com/example/uscoding/dto/PostResponseDto.java
+++ src/main/java/com/example/uscoding/dto/PostResponseDto.java
@@ -1,3 +1,36 @@
 package com.example.uscoding.dto;
 
-public record PostResponseDto(Long id) {}
+import com.example.uscoding.domain.Post;
+import com.example.uscoding.domain.UserAccount;
+import lombok.Builder;
+import org.springframework.security.core.parameters.P;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public record PostResponseDto(
+        Long id,
+        UserAccount user,
+        String title,
+        String code,
+        String content,
+        String category,
+        LocalDate date,
+        Long likes
+) {
+    @Builder
+    public PostResponseDto {}
+
+    public static PostResponseDto from(Post p){
+        return new PostResponseDto(
+                p.getId(),
+                p.getUser(),
+                p.getTitle(),
+                p.getCode(),
+                p.getContent(),
+                p.getCategory(),
+                p.getDate() != null ? p.getDate() : null ,
+                p.getLikes()
+        );
+    }
+}
